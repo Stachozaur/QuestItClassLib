2a77ccd64db9ee49772c6e97b880c27f90818540
Stachozaur <stachowicz.karol@gmail.com>
2021-03-31 12:08:03 +0200

    Dependency Injection in Repo

--- Data/DAL/QuestRepository.cs
+++ Data/DAL/QuestRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Job.it_classes.Data.Context;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,13 @@ namespace Job.it_classes.Data.DAL
 {
     public class QuestRepository
     {
-        
+        private readonly BaseDbContext _context;
+        private readonly ILogger<QuestRepository> _logger;
+
+        public QuestRepository(BaseDbContext context, ILogger<QuestRepository> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
     }
 }
